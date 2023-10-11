fea4f51bbe9b753a16cb50f28aaaab98eca850b1
Haddocker <a.riisovergaard@gmail.com>
2023-10-11 13:58:19 +0200

    Day 1: main.py

--- Udemy - 100 Days of Code/Day_001/main.py
+++ Udemy - 100 Days of Code/Day_001/main.py
@@ -1,3 +1,13 @@
-print("Day 1 - Python Print Function")
-print("The function is declared like this: ")
-print("print('what to print')")
+### Print function:
+
+#print("Day 1 - Python Print Function")
+#print("The function is declared like this: ")
+#print("print('what to print')")
+
+
+### Input function:
+input("A prompt for the user")
+print("Hello " + input("What is your name? "))
+
+variable = input("User input")
+print(variable)
