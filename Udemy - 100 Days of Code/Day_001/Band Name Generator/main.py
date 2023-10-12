bd4a8f738df3d3badae742aa1a0dedb64d5ce330
Haddocker <a.riisovergaard@gmail.com>
2023-10-12 11:13:34 +0200

    Day 1: Band name generator DONE

---
+++ Udemy - 100 Days of Code/Day_001/Band Name Generator/main.py
@@ -0,0 +1,7 @@
+print("Hi! In this program we will create a band name for you!\n")
+
+city_name = (input("What is the name of the city you grew up in?: "))
+
+pet_name = (input("What is the name of your pet?: "))
+
+print("\nThis is your new band name: " + city_name + " " + pet_name + "!")
