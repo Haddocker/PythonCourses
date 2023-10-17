942f9f48d348b4826c7a10607a9bc1045240b4c2
Haddocker <a.riisovergaard@gmail.com>
2023-10-17 10:46:26 +0200

    Day 2: Life in Weeks DONE

---
+++ Udemy - 100 Days of Code/Day_002/Life In Weeks/main.py
@@ -0,0 +1,23 @@
+age = input()
+
+# 1 year = 52 weeks
+# Average age 90 years
+
+
+
+# My code
+total_age_in_weeks = 90 * 52
+weeks_lived = int(age) * 52
+
+weeks_left = total_age_in_weeks - weeks_lived
+
+print(f"You have {weeks_left} weeks left to live...")
+
+
+
+
+# Udemy Code:
+years = 90 - int(age)
+weeks = years * 52
+
+print(f"You have {weeks} left.")
