eaed106f88773adb2d5f816764cc6fcb6f0dee35
Haddocker <a.riisovergaard@gmail.com>
2023-10-19 08:57:53 +0200

    Day 2: Tip Calculator FINAL

--- Udemy - 100 Days of Code/Day_002/Tip Calculator/main.py
+++ Udemy - 100 Days of Code/Day_002/Tip Calculator/main.py
@@ -4,11 +4,24 @@
 
 print("Welcome to the tip calculator\n")
 
-bill = input("What was the total bill?: $")
-tip = input("What percentage tip would you like to give ? 10, 12, or 15?: ")
-people = input("How many people to split the bill?: ")
+bill = float(input("What was the total bill?: $"))
+tip = int(input("What percentage tip would you like to give ? 10, 12, or 15?: "))
+people = int(input("How many people to split the bill?: "))
 
 
-total_bill = (bill / tip) * (1 * (100))
+# MY CODE:
+# bill_pr_person = round((bill / people) * (1 + (tip / 100)), 2)
+# total_bill = round((bill_pr_person * people), 2)
+#
+# print(f"The total bill is ${total_bill}")
+# print(f"and you all have to pay ${bill_pr_person} each.")
 
-bill_
+
+# UDEMY CODE:
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+final_amount = ":.2f".format(bill_per_person)
+print(f"Each person should pay: ${final_amount}")
