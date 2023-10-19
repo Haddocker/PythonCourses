85211e3ad21326b4b82e6cf392511af7a714c0cb
Haddocker <a.riisovergaard@gmail.com>
2023-10-19 14:29:57 +0200

    Day 3: Lear Year Setup

--- Udemy - 100 Days of Code/Day_003/Leap Year/main.py
+++ Udemy - 100 Days of Code/Day_003/Leap Year/main.py
@@ -2,4 +2,9 @@
 # EXCEPT every year that is evenly divisible by 100 with no remainder.
 # UNLESS the year is also divisible by 400 with no remainder.
 
-year
+year = int(input("Insert a year: "))
+
+if year / 4 % 2 == 1:
+    print("Not a leap year.")
+else:
+    if
