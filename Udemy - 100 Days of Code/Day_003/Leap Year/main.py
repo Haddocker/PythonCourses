033377c9cef70c2fb10fc0cc703d471feec15333
Haddocker <a.riisovergaard@gmail.com>
2023-10-19 14:14:03 +0200

    Day 3: Lear Year Setup

---
+++ Udemy - 100 Days of Code/Day_003/Leap Year/main.py
@@ -0,0 +1,5 @@
+# On every year that is divisible by 4 with no remainder.
+# EXCEPT every year that is evenly divisible by 100 with no remainder.
+# UNLESS the year is also divisible by 400 with no remainder.
+
+year
