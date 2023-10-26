5e3bc4019e6aec4cadfa0e0bae1fa071d01de3cc
Haddocker <a.riisovergaard@gmail.com>
2023-10-26 22:31:29 +0200

    Day 4: Banker Roulette FINAL

---
+++ Udemy - 100 Days of Code/Day_004/Banker Roulette/main.py
@@ -0,0 +1,16 @@
+names_string = input("Write all your names comma separated: ")
+
+names = names_string.split(", ")
+
+import random
+
+# Get the total number of item in list.
+num_items = len(names)
+
+# Generate random numbers between 0 and the last index.
+random_choice = random.randint(0, num_items - 1)
+
+# Pick out a random person from list of names using the random number.
+person_who_will_pay = names[random_choice]
+
+print(person_who_will_pay + " is going to pay for the meal today!")
