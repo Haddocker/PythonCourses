db91ca351215bf9f626a982c38beeb0d010e9f79
Haddocker <a.riisovergaard@gmail.com>
2023-11-02 15:32:24 +0100

    Day 7: Update

--- Udemy - 100 Days of Code/Day_007/Hangman/main.py
+++ Udemy - 100 Days of Code/Day_007/Hangman/main.py
@@ -1,15 +1,14 @@
-word_list = ["ardvark", "baboon", "camel"]
+import random
 
 print("Welcome to a game of Hangman!")
 
+word_list = ["ardvark", "baboon", "camel"]
 
-# Generate a random word:
+# TODO-1 - Random choose a word from the word_list and assign it to a variable called chosen_word
+chosen_word = random.choice(word_list)
 
+# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Guess a letter: ").lower()
 
-# Generate as many blanks as letters in word:
-
-
-# Ask the user to guess a letter:
-
-
-#
+# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+chosen_word_list = list(chosen_word)
