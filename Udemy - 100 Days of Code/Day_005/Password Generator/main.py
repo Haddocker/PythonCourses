7426552f59d411c13b7ad59991b4a8767f5b324f
Haddocker <a.riisovergaard@gmail.com>
2023-11-02 14:15:24 +0100

    Day 5: Password Generator UPDATE

--- Udemy - 100 Days of Code/Day_005/Password Generator/main.py
+++ Udemy - 100 Days of Code/Day_005/Password Generator/main.py
@@ -4,10 +4,22 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
+print("Welcome to the Password Generator!")
+
 nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# Capture if user input is 0 - NOT DONE.
+if nr_letters < 2:
+    print("You have to have a least 2 letters! Try again.")
+    nr_letters = int(input("How many letters would you like in your password?\n"))
+if nr_symbols < 2:
+    print("You have to have at least 2 symbols! Try again.")
+    nr_symbols = int(input(f"How many symbols would you like?\n"))
+if nr_numbers < 2:
+    print("You have to have at least 2 numbers! Try again.")
+    nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 
 # Eazy Level - Order not randomised:
