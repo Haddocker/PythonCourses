c815af91e1bfb04cb6404edfe5a10801ad4f2217
Haddocker <a.riisovergaard@gmail.com>
2023-11-07 17:27:07 +0100

    Day 7: Hangman FINAL

--- Udemy - 100 Days of Code/Day_007/Hangman/main.py
+++ Udemy - 100 Days of Code/Day_007/Hangman/main.py
@@ -1,19 +1,61 @@
 import random
+# import os
+from hangman_art import logo, stages
+from hangman_words import word_list
 
+
+# Faking a clear all..
+def clear_screen():
+    print('\n' * 100)
+
+
+print(logo)
 print("Welcome to a game of Hangman!")
 
-word_list = ["ardvark", "baboon", "camel"]
-
-# TODO-1 - Random choose a word from the word_list and assign it to a variable called chosen_word
 chosen_word = random.choice(word_list)
+word_len = len(chosen_word)
+game_over = False
+lives = 6
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ").lower()
+# Create blanks
+display = []
+for _ in range(word_len):
+    display += "_"
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+print(f"{' '.join(display)}")
+
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+
+    # Calling clear_screen function
+    clear_screen()
+
+    # Check if letter is already guessed.
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    # Check guessed letter.
+    for position in range(word_len):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    # Check if minus lives or lose.
+    if guess not in display:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print("You lose!")
+
+    # Join all elements in the list and turn into string.
+    print(f"{' '.join(display)}")
+
+    # Check if win.
+    if "_" not in display:
+        game_over = True
+        print("You win!")
+
+    print(stages[lives])
+
