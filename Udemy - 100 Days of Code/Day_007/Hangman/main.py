2f848692de77d73ca37610efca610524bac9f472
Haddocker <a.riisovergaard@gmail.com>
2023-11-07 08:56:47 +0100

    Day 7: Hangman Update

--- Udemy - 100 Days of Code/Day_007/Hangman/main.py
+++ Udemy - 100 Days of Code/Day_007/Hangman/main.py
@@ -11,4 +11,9 @@ chosen_word = random.choice(word_list)
 guess = input("Guess a letter: ").lower()
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-chosen_word_list = list(chosen_word)
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
