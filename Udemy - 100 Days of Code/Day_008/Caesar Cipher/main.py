57a1b57ad16b1c132d5a24d6e6ad0a72ce37a8f1
Haddocker <a.riisovergaard@gmail.com>
2023-11-28 21:51:30 +0100

    Day 8: Caesar Cipher FINAL

--- Udemy - 100 Days of Code/Day_008/Caesar Cipher/main.py
+++ Udemy - 100 Days of Code/Day_008/Caesar Cipher/main.py
@@ -1,28 +1,41 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-from art
+from art import logo
+print(logo)
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-
-# NEW
 def caesar(cipher_direction, start_text, shift_amount):
     end_text = ""
     if cipher_direction == "decode":
         shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
+    # Encoding/decoding only alphabetic characters.
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
     print(f"The {cipher_direction}d message is {end_text}")
 
-caesar(cipher_direction=direction, start_text=text, shift_amount=shift)
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    # Making sure the shift number is in the alphabet range.
+    shift = shift % 26
+
+    caesar(cipher_direction=direction, start_text=text, shift_amount=shift)
+
+    try_again = input("Do you want to do it again? Type 'Y' or 'N':\n").lower()
+    if try_again == "n":
+        should_continue = False
+        print("Quitting the Caesar Cipher.")
 
 
-# OLD
 
+# OLD CODE
 # def encrypt(plain_text, shift_amount):
 #     cipher_text = ""
 #     for letter in plain_text:
